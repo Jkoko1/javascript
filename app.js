cd9109aaee681474d8402c8249e2ae7cfe34dd78
Jkoko1 <jerryspencer91@gmail.com>
2022-04-14 13:13:27 +0100

    javascript

---
+++ app.js
@@ -0,0 +1,23 @@
+var num1 = parseInt(prompt("Enter num1"));
+
+var num2 = parseInt(prompt("Enter num2"));
+
+var total = num1 + num2;
+
+alert(total);
+
+var firstname = prompt("Enter your firstname");
+
+var first = firstname.slice(0,4);
+
+var firstn = first.toLowerCase();
+
+var lastname = prompt("Enter your lastname");
+
+var last = lastname.slice(0,1);
+
+var lastn = last.toLowerCase();
+
+var userid = "Your CapacityBay userid is " + firstn + lastn;
+
+alert(userid);
